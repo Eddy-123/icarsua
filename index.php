a4eef2178962c9f49d94fa318162c17818520552
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-01-20 22:57:53 +0100

    top menu created

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Icarsua</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <script type="text/javascript" src="js/index.js" defer></script>
+  </head>
+  <body>
+
+    <div class="" id="top"><!-- #top begin -->
+
+      <div class="container"><!-- container begin -->
+
+        <div class="col-md-6"><!-- col-md-6 begin -->
+          <p>Icarsua</p>
+        </div><!-- col-md-6 end -->
+
+        <div class="col-md-6"><!-- col-md-6 begin -->
+
+          <ul class="menu">
+            <li> <a href="customer_registration.php">Inscription</a> </li>
+            <li> <a href="checkout.php">Connexion</a> </li>
+            <li> <a href="cart.php">Panier</a> </li>
+          </ul>
+
+        </div><!-- col-md-6 end -->
+
+      </div><!-- container end -->
+
+    </div><!-- #top end -->
+  </body>
+</html>
