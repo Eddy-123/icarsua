4af6cc72472b6c8f66c65942c778fbfca74d186e
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-01-21 16:53:35 +0100

    main menu added

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Icarsua</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <script type="text/javascript" src="js/index.js" defer></script>
   </head>
@@ -21,7 +22,7 @@
 
           <ul class="menu">
             <li> <a href="customer_registration.php">Inscription</a> </li>
-            <li> <a href="checkout.php">Connexion</a> </li>
+            <li> <a href="checkout.php">Connection</a> </li>
             <li> <a href="cart.php">Panier</a> </li>
           </ul>
 
@@ -30,5 +31,37 @@
       </div><!-- container end -->
 
     </div><!-- #top end -->
+
+    <nav class="navbar navbar-default" id="navbar">
+    
+      <div class="container">
+
+        <a href="index.php" class="navbar-brand">LOGO</a>
+        
+        <div class="" id="navigation">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="index.php">Acceuil</a></li>
+              <li><a href="shop.php">Boutique</a></li>
+              <li><a href="checkout.php">Compte</a></li>
+              <li><a href="cart.php">Panier</a></li>
+              <li><a href="contact.php">Contact</a></li>
+            </ul>
+          
+            <!-- Search form -->
+            <form class="form-inline md-form form-sm" action="results.php" method="get">
+              <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Recherche"
+                aria-label="Search" required>
+                <button type="submit" ><i type="submit" class="fa fa-search" aria-hidden="true"></i></button>
+            </form>
+          
+          </div>
+
+          </div>
+        </div>
+
+      </div>
+    
+    </nav>
+    
   </body>
 </html>
