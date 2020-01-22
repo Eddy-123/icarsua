6a97bb524d05e5b90012a7f320277de9ed2bebdf
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-01-22 23:39:16 +0100

    slider created

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <title>Icarsua</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js" defer></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" defer></script>
     <link rel="stylesheet" href="./css/style.css">
     <script type="text/javascript" src="js/index.js" defer></script>
   </head>
@@ -62,6 +65,40 @@
       </div>
     
     </nav>
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="admin_area/slides_images/slide-1.jpg" alt="Los Angeles" style="width:100%">
+        </div>
+
+        <div class="item">
+          <img src="admin_area/slides_images/slide-1.jpg" alt="Chicago" style="width:100%">
+        </div>
+
+        <div class="item">
+          <img src="admin_area/slides_images/slide-1.jpg" alt="New York" style="width:100%">
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
+  </div>    
     
   </body>
 </html>
