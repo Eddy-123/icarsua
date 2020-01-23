8127f3c9bff574f991791027ec02a3f6155fef97
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-01-23 17:33:25 +0100

    carousel fixed

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Icarsua</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" id="bootstrap-css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+    
+    
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js" defer></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" defer></script>
@@ -66,39 +71,50 @@
     
     </nav>
 
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <section class="section-white">
+  <div class="container">
+
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
       </ol>
 
       <!-- Wrapper for slides -->
       <div class="carousel-inner">
         <div class="item active">
-          <img src="admin_area/slides_images/slide-1.jpg" alt="Los Angeles" style="width:100%">
+          <img src="admin_area/slides_images/slide-1.png" alt="...">
+          <div class="carousel-caption">
+            <h2></h2>
+          </div>
         </div>
-
         <div class="item">
-          <img src="admin_area/slides_images/slide-1.jpg" alt="Chicago" style="width:100%">
+          <img src="admin_area/slides_images/slide-2.png" alt="...">
+          <div class="carousel-caption">
+            <h2></h2>
+          </div>
         </div>
-
         <div class="item">
-          <img src="admin_area/slides_images/slide-1.jpg" alt="New York" style="width:100%">
+          <img src="admin_area/slides_images/slide-3.png" alt="...">
+          <div class="carousel-caption">
+            <h2></h2>
+          </div>
         </div>
       </div>
 
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
       </a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
       </a>
-  </div>    
+    </div>
+
+  </div>
+</section>    
     
   </body>
 </html>
