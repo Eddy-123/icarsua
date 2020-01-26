1018aa0fc8328c597207f48909ff44515cd39817
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-01-26 17:33:20 +0100

    product storefront

--- index.php
+++ index.php
@@ -71,50 +71,237 @@
     
     </nav>
 
-    <section class="section-white">
-  <div class="container">
+  <section class="section-white" id="slider">
+    <div class="container">
 
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      </ol>
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="admin_area/slides_images/slide-1.png" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="admin_area/slides_images/slide-1.png" alt="...">
+            <div class="carousel-caption">
+              <h2></h2>
+            </div>
+          </div>
+          <div class="item">
+            <img src="admin_area/slides_images/slide-2.png" alt="...">
+            <div class="carousel-caption">
+              <h2></h2>
+            </div>
+          </div>
+          <div class="item">
+            <img src="admin_area/slides_images/slide-3.png" alt="...">
+            <div class="carousel-caption">
+              <h2></h2>
+            </div>
           </div>
         </div>
-        <div class="item">
-          <img src="admin_area/slides_images/slide-2.png" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
-          </div>
-        </div>
-        <div class="item">
-          <img src="admin_area/slides_images/slide-3.png" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+      </div>
+
+    </div>
+  </section>    
+  
+  <h2>Les nouvelles voitures</h2>
+  <div class="container-fluid" id="content">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-1.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
           </div>
         </div>
       </div>
-
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-2.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-3.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-3.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-1.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-2.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-3.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-3.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-1.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-2.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-3.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="product">
+          <a href="details.php">
+          <img class="img-responsive" src="admin_area/product_images/product-3.png" alt="product 1">
+          </a>
+          <div class="text">
+            <h3>
+              <a href="details.php">Honda</a>
+            </h3>
+            <p class="price">
+              500£   
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-
   </div>
-</section>    
-    
+
+
   </body>
 </html>
