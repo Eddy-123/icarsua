3ea0833383c2147aba9026a5c2565ef78e329d67
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-02-06 19:34:13 +0100

    creation of register page

--- customer_registration.php
+++ customer_registration.php
@@ -48,11 +48,11 @@
         
         <div class="" id="navigation">
             <ul class="nav navbar-nav">
-              <li><a href="index.php">Acceuil</a></li>
+              <li class="active"><a href="index.php">Acceuil</a></li>
               <li><a href="shop.php">Boutique</a></li>
               <li><a href="checkout.php">Compte</a></li>
               <li><a href="cart.php">Panier</a></li>
-              <li class="active"><a href="contact.php">Contact</a></li>
+              <li><a href="contact.php">Contact</a></li>
             </ul>
           
             <!-- Search form -->
@@ -74,7 +74,7 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="index.php">Acceuil</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Contact</li>
+            <li class="breadcrumb-item active" aria-current="page">Inscription</li>
         </ol>
     </nav>
 
@@ -85,30 +85,47 @@
     <div class="col-md-9">
         <center>        
             <h3 class="text-muted">
-                N'hésitez pas à nous contacter !
+                Inscription
             </h3>
-            <form action="contact.php" method="post">
+        </center>
+            <form action="contact.php" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label for="name">Nom</label>
-                    <input type="text" class="form-control" name="name" required>
+                    <label for="c_name">Votre nom</label>
+                    <input type="text" class="form-control" name="c_name" required>
                 </div>
                 <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" name="email" required>
+                    <label for="email">Votre Email</label>
+                    <input type="email" class="form-control" name="c_email" required>
                 </div>
                 <div class="form-group">
-                    <label for="subject">Sujet</label>
-                    <input type="text" class="form-control" name="subject" required>
+                    <label for="subject">Votre mot de passe</label>
+                    <input type="password" class="form-control" name="c_pass" required>
                 </div>
                 <div class="form-group">
-                    <label for="message">Message</label>
-                    <textarea name="message" class="form-control"></textarea>
+                    <label for="c_country">Votre pays</label>
+                    <input type="text" class="form-control" name="c_country" required>
+                </div>
+                <div class="form-group">
+                    <label for="c_city">Votre ville</label>
+                    <input type="text" class="form-control" name="c_city" required>
+                </div>
+                <div class="form-group">
+                    <label for="c_contact">Votre contact</label>
+                    <input type="text" class="form-control" name="c_contact" required>
+                </div>
+                <div class="form-group">
+                    <label for="c_address">Votre adresse</label>
+                    <input type="text" class="form-control" name="c_address" required>
+                </div>
+                <div class="form-group">
+                    <label for="c_image">Votre photo de profil</label>
+                    <input type="file" class="form-control" name="c_image" required>
                 </div>
                 <div class="text-center">
-                    <button type="submit" name="submit">Envoyer</button>
+                    <button type="submit" name="register">S'inscrire</button>
                 </div>
             </form>
-        </center>
+        
     </div>
 
     <?php include("includes/footer.php"); ?>
