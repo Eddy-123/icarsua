f309a7d38e939f6b8d3ee4086392b7f5da37841d
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-02-12 20:58:34 +0100

    working on confirm page

--- customer/my_orders.php
+++ customer/my_orders.php
@@ -1,3 +1,66 @@
 <center>
     <h1>Mes commandes</h1>
 </center>
+
+<div class="table-responsive">
+    <table class="table-bordered table-hover orders">
+        <thead>
+            <tr>
+                <th>Sur</th>
+                <th>Coût</th>
+                <th>Facture</th>
+                <th>Quantité</th>
+                <th>Taille</th>
+                <th>Date de commande</th>
+                <th>Payé / Non payé</th>
+                <th>Statut</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <th> #1 </th>
+                <td> 500£ </td>
+                <td> 123456789 </td>
+                <td> 2 </td>
+                <td> Petit </td>
+                <td> 12-02-20 </td>
+                <td> Non payé </td>
+                <td><a href="confirm.php" target="_blank" class="btn btn-default btn-sm"> Confirmer payement </a></td>
+            </tr>
+
+            <tr>
+                <th> #1 </th>
+                <td> 500£ </td>
+                <td> 123456789 </td>
+                <td> 2 </td>
+                <td> Petit </td>
+                <td> 12-02-20 </td>
+                <td> Non payé </td>
+                <td><a href="confirm.php" target="_blank" class="btn btn-default btn-sm"> Confirmer payement </a></td>
+            </tr>
+
+            <tr>
+                <th> #1 </th>
+                <td> 500£ </td>
+                <td> 123456789 </td>
+                <td> 2 </td>
+                <td> Petit </td>
+                <td> 12-02-20 </td>
+                <td> Non payé </td>
+                <td><a href="confirm.php" target="_blank" class="btn btn-default btn-sm"> Confirmer payement </a></td>
+            </tr>
+
+            <tr>
+                <th> #1 </th>
+                <td> 500£ </td>
+                <td> 123456789 </td>
+                <td> 2 </td>
+                <td> Petit </td>
+                <td> 12-02-20 </td>
+                <td> Non payé </td>
+                <td><a href="confirm.php" target="_blank" class="btn btn-default btn-sm"> Confirmer payement </a></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
