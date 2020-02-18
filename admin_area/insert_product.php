cd9d234db4825253dfc8bd5db900432135f0c019
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-02-18 19:48:55 +0100

    create products tables, working on insertion file

---
+++ admin_area/insert_product.php
@@ -0,0 +1,16 @@
+<?php
+include("includes/db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Insérer les Produits</title>
+</head>
+<body>
+    
+</body>
+</html>
