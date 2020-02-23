ec80ccd15f120082f839cdd295fae2b925271ed3
Eddy-123 <adegnandjoueddy12@gmail.com>
2020-02-23 22:53:04 +0100

    insertion form

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -12,12 +12,12 @@ include("includes/db.php");
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" id="bootstrap-css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-      
-    
-    
+
+
+
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    
-    
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js" defer></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" defer></script>
     <link rel="stylesheet" href="./css/style.css">
@@ -92,12 +92,53 @@ include("includes/db.php");
                         </div>
 
                         <div class="form-group">
-                            <label for="" class="col-md-3 control-label">Image de la voiture</label>
+                            <label for="" class="col-md-3 control-label">Première image de la voiture</label>
                             <div class="col-md-6">
                                 <input name="product_img1" type="file" class="form-control" required>
                             </div>
                         </div>
 
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label">Première image de la voiture</label>
+                            <div class="col-md-6">
+                                <input name="product_img1" type="file" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label">Deuxième image de la voiture</label>
+                            <div class="col-md-6">
+                                <input name="product_img2" type="file" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label">Troisième image de la voiture</label>
+                            <div class="col-md-6">
+                                <input name="product_img3" type="file" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label">Prix de la voiture</label>
+                            <div class="col-md-6">
+                                <input name="product_price" type="text" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label">Mots clés de la voiture</label>
+                            <div class="col-md-6">
+                                <input name="product_keywords" type="text" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label">Description de la voiture</label>
+                            <div class="col-md-6">
+                                <textarea name="product_desc" rows="8" cols="80" class="form-control"></textarea>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
